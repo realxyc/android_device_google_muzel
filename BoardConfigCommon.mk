c6424701dabd09d0f3323a1292aa01bc93613d2c
realxyc <caesar.x.julius@gmail.com>
2026-07-02 00:05:23 +0530

    muzel: Update SP from CP1A.260505.005
    Change-Id: I9c593037f4a76ec18d10697ceb211f291c7fae35
    Signed-off-by: realxyc <caesar.x.julius@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,7 +5,7 @@
 #
 
 # Security - must be defined before including BoardConfig-common.mk
-BOOT_SECURITY_PATCH := 2026-02-05
+BOOT_SECURITY_PATCH := 2026-05-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 include device/google/laguna/BoardConfig-common.mk
